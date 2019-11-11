ba7cc2f9bf291db3e6f2390835e02591aef3cf73
eo39 <zhekaonichsenko@gmail.com>
2019-11-11 23:13:50 +0200

    Update Figure.cs
    Упростил методы поворота фигуры

--- Tetris/Figure.cs
+++ Tetris/Figure.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
@@ -6,7 +7,7 @@ namespace Tetris
 {
     internal class Figure
     {
-        public readonly Point[] Cells;
+        public Point[] Cells { get; private set; }
 
         private static readonly Random Random = new Random();
         private readonly FigureType figureType;
@@ -24,28 +25,28 @@ namespace Tetris
             switch (Random.Next(8))
             {
                 case 0:
-                    return new Figure(FigureType.T, 0,
+                    return new Figure(FigureType.T, 1,
                         new[] {new Point(5, 0), new Point(5, 1),new Point(5, 2), new Point(4, 2)});
                 case 1:
-                    return new Figure(FigureType.J, 0,
+                    return new Figure(FigureType.J, 1,
                         new[] { new Point(4, 0), new Point(4, 1), new Point(3, 1), new Point(5, 1) });
                 case 2:
-                    return new Figure(FigureType.L, 0,
+                    return new Figure(FigureType.L, 1,
                         new[] { new Point(4, 0), new Point(4, 1), new Point(4, 2), new Point(5, 2) });
                 case 3:
-                    return new Figure(FigureType.Z, 0,
+                    return new Figure(FigureType.Z, 1,
                         new[] { new Point(3, 0), new Point(4, 1), new Point(4, 0), new Point(5, 1) });
                 case 4:
-                    return new Figure(FigureType.S, 0,
+                    return new Figure(FigureType.S, 1,
                         new[] { new Point(5, 0), new Point(4, 1), new Point(4, 0), new Point(3, 1) });
                 case 5:
-                    return new Figure(FigureType.I, 0,
+                    return new Figure(FigureType.I, 1,
                         new[] { new Point(5, 0), new Point(4, 0), new Point(6, 0), new Point(3, 0) });
                 case 6:
-                    return new Figure(FigureType.O, 0,
+                    return new Figure(FigureType.O, 1,
                         new[] { new Point(4, 0), new Point(4, 1), new Point(5, 1), new Point(5, 0) });
                 case 7:
-                    return new Figure(FigureType.Point, 0,
+                    return new Figure(FigureType.Point, 1,
                         new[] { new Point(4, 0), new Point(4, 0), new Point(4, 0), new Point(4, 0) });
                 default: throw new ArgumentOutOfRangeException();
             }
@@ -70,63 +71,39 @@ namespace Tetris
                 case FigureType.T:
                 case FigureType.J:
                 case FigureType.L:
-                    RotateCoordinates("Right");
+                    Cells = GetRotatedCoordinates();
                     break;
 
                 case FigureType.Z:
                 case FigureType.S:
                 case FigureType.I:
-                    RotateCoordinates();
+                    Cells = GetRotatedCoordinates();
+                    rotateMode = rotateMode == 1 ? -1 : 1;
                     break;
             }
 
-            if (!CanFigureRotate(gameField, gameFieldWidth, gameFieldHeight))
+            if (!CanFigureRotate(Cells, gameField, gameFieldWidth, gameFieldHeight))
                 Array.Copy(currentFigureTemp, Cells, Cells.Length);
         }
 
-        private bool CanFigureRotate(bool[,] gameField, int gameFieldWidth, int gameFieldHeight)
+        private static bool CanFigureRotate(IEnumerable<Point> cells, bool[,] gameField, int gameFieldWidth, int gameFieldHeight)
         {
-            return Cells.All(point => point.X < gameFieldWidth && 
-                                      point.X >= 0 && 
-                                      point.Y < gameFieldHeight && 
-                                      point.Y >= 0 && 
+            return cells.All(point => point.X < gameFieldWidth &&
+                                      point.X >= 0 &&
+                                      point.Y < gameFieldHeight &&
+                                      point.Y >= 0 &&
                                       !gameField[point.X, point.Y]);
         }
 
-        private void RotateCoordinates(string direction)
+        private Point[] GetRotatedCoordinates()
         {
-            switch (direction)
-            {
-                case "Right":
-                {
-                    for (int i = 0; i < Cells.Length; i++)
-                        Cells[i] = new Point(Cells[1].X + Cells[1].Y - Cells[i].Y, Cells[1].Y - Cells[1].X + Cells[i].X);
+            Point[] coordinatesTemp = Cells;
 
-                    break;
-                }
+            for (int i = 0; i < Cells.Length; i++)
+                coordinatesTemp[i] = new Point(Cells[1].X + (Cells[1].Y - Cells[i].Y) * rotateMode,
+                    coordinatesTemp[1].Y + (Cells[i].X - Cells[1].X) * rotateMode);
 
-                case "Left":
-                {
-                    for (int i = 0; i < Cells.Length; i++)
-                        Cells[i] = new Point(Cells[1].X - Cells[1].Y + Cells[i].Y, Cells[1].Y + Cells[1].X - Cells[i].X);
-
-                    break;
-                }
-            }
-        }
-
-        private void RotateCoordinates()
-        {
-            if (rotateMode == 1)
-            {
-                RotateCoordinates("Right");
-                rotateMode = 2;
-            }
-            else
-            {
-                RotateCoordinates("Left");
-                rotateMode = 1;
-            }
+            return coordinatesTemp;
         }
     }
 }
